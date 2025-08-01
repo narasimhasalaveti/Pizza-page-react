7bea14d88c5f21b9fd0421e8661186fb9cc56c1f
salaveti.Reddy <6o1QLc5hi1blT0MvrtASl0e9nsFoWLJOL1J5xWSNH7ZaYfv0ReAbJQQJ99AKACAAAAAq259BAAASAZDOFcvr>
2025-08-01 16:23:25 +0530

    Pizza page project files

---
+++ src/data.js
@@ -0,0 +1,47 @@
+const pizzaData = [
+	{
+		name: "Focaccia",
+		ingredients: "Bread with italian olive oil and rosemary",
+		price: 60,
+		photoName: "pizzas/focaccia.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Margherita",
+		ingredients: "Tomato and mozarella",
+		price: 100,
+		photoName: "pizzas/margherita.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Spinaci",
+		ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+		price: 120,
+		photoName: "pizzas/spinaci.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Funghi",
+		ingredients: "Tomato, mozarella, mushrooms, and onion",
+		price: 120,
+		photoName: "pizzas/funghi.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Salamino",
+		ingredients: "Tomato, mozarella, and pepperoni",
+		price: 150,
+		photoName: "pizzas/salamino.jpg",
+		soldOut: true,
+	},
+	{
+		name: "Pizza Prosciutto",
+		ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+		price: 180,
+		photoName: "pizzas/prosciutto.jpg",
+		soldOut: false,
+	},
+];
+
+// Export pizzaData
+export default pizzaData;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,122 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import pizzaData from "./data.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+	return (
+		<div className="container">
+			<Header />
+			<Menu />
+			<Footer />
+		</div>
+	);
+}
+
+function Header() {
+	return (
+		<header className="header">
+			<h1>Fast React Pizza Co.</h1>
+		</header>
+	);
+}
+
+function Menu() {
+	const pizzas = pizzaData;
+	return (
+		<main className="menu">
+			<h2>Our Menu</h2>
+			{pizzas ? (
+				<React.Fragment>
+					<p>
+						Authentic Italian cuisine. 6 creative pizzas to choose from. All
+						from our stone oven, all organic, all delicious.
+					</p>
+					<ul className="pizzas">
+						{pizzas.map((pizza) => (
+							<Pizza pizzaObj={pizza} key={pizza.name} />
+						))}
+					</ul>
+				</React.Fragment>
+			) : (
+				<h1>Sorry, we're still working on our menu. Please come back later!</h1>
+			)}
+			{/* <Pizza
+				name="Pizza Spinaci"
+				ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+				photoName="pizzas/spinaci.jpg"
+				price={120}
+			/>
+			<Pizza
+				name="Pizza Margherita"
+				ingredients="Tomato and mozarella"
+				photoName="pizzas/margherita.jpg"
+				price={100}
+			/> */}
+		</main>
+	);
+}
+
+// instead of passing as props, we are destructuring the pizza object
+function Pizza({ pizzaObj }) {
+	return (
+		<li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+			<img src={pizzaObj.photoName} alt={pizzaObj.name} />
+			<div>
+				<h3>{pizzaObj.name}</h3>
+				<p>{pizzaObj.ingredients}</p>
+				<span>
+					{pizzaObj?.soldOut ? "SOLD OUT" : "₹" + (pizzaObj.price + 9)}
+				</span>
+			</div>
+		</li>
+	);
+}
+
+function Footer() {
+	const hour = new Date().getHours();
+	const openHour = 12;
+	const closedHour = 23;
+	const isOpen = hour >= openHour && hour <= closedHour;
+
+	return (
+		<footer className="footer">
+			{isOpen ? (
+				<OrderOpens closedHour={closedHour} />
+			) : (
+				<OrderClosed openHour={openHour} closedHour={closedHour} />
+			)}
+		</footer>
+	);
+}
+
+// Here we used props
+function OrderOpens(props) {
+	return (
+		<div className="order">
+			<p>
+				We're open until {props.closedHour}:00. Come visit us or order online
+			</p>
+			<button className="btn">Order</button>
+		</div>
+	);
+}
+
+// instead of passing as props, we are destructuring the values
+function OrderClosed({ openHour, closedHour }) {
+	return (
+		<div className="order">
+			<p>
+				We're happy to welcome you between {openHour}:00 and {closedHour}
+				:00. Please come back later😊!
+			</p>
+		</div>
+	);
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
